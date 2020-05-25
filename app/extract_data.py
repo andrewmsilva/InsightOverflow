043ac531b0baa9e06d86b4c074b45d13240c66bc
Andrew M. Silva <andrewsaxx@gmail.com>
2020-05-25 00:23:32 -0300

    Minor updates

--- app/extract_data.py
+++ app/extract_data.py
@@ -4,15 +4,19 @@ from time import time
 
 start_time = time()
 
-data_path = '../data/posts.xml'
-context = etree.iterparse(data_path, tag='row')
+# Create XML context
+data_path = '../data/'
+context = etree.iterparse(data_path+'posts.xml', tag='row')
 
+# Settings
 years = []
 extracted_count = 0
 total_count = 0
 
+# Get posts
 for event, element in context:
     total_count += 1
+    # Filter posts
     post_type = element.get('PostTypeId')
     if 'OwnerUserId' in element.keys() and (post_type is '1' or post_type is '2'):
         # Get values
@@ -23,16 +27,16 @@ for event, element in context:
             'author': element.get('OwnerUserId'),
             'content': element.get('Body')
         }
+        # Append title
         if 'Title' in element.keys():
             post['content'] += element.get('Title')
-        # Write in CSV       
-        with open('../data/{}-posts.csv'.format(post['year']), 'a') as f:
+        # Write in CSV
+        with open(data_path+'{}-posts.csv'.format(post['year']), 'a') as f:
             writer = DictWriter(f, fieldnames=post.keys()) 
             if post['year'] not in years:
                 writer.writeheader()
                 years.append(post['year'])
             writer.writerow(post)
-            f.close()
     # Clear memory
     element.clear()
     for ancestor in element.xpath('ancestor-or-self::*'):
