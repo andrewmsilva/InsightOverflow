b44fda4afacdc716ece983afcfb65d44601c88ce
Andrew M. Silva <andrewsaxx@gmail.com>
2020-06-21 20:33:51 -0300

    Optimizing extraction

--- app/extraction.py
+++ app/extraction.py
@@ -9,7 +9,7 @@ start_time = time()
 # Settings
 path = '../data/'
 source_file = 'posts.xml'
-results_file = 'posts>=0.csv'
+results_file = 'posts.csv'
 columns = ['id', 'date', 'author', 'body']
 
 # Connect to Redis
@@ -21,39 +21,42 @@ with open(path+results_file, 'w', errors='surrogatepass') as f:
     writer.writeheader()
 
 # Get posts
-extracted_count= 0
+extracted_count = 0
 total_count = 0
 for event, element in etree.iterparse(path+source_file, tag='row'):
     total_count += 1
     # Filter questions and answers
     post_type = int(element.get('PostTypeId'))
     if post_type == 1 or post_type == 2:
-        # Get values
-        post = {
-            'id': element.get('Id'),
-            'date': element.get('CreationDate')[:10],
-            'author': element.get('OwnerUserId'),
-            'body': element.get('Body')
-        }
-        # If it is a question, append title and tags
+        # Get and save tags
+        tags = ' '
         if post_type == 1:
-            tags = element.get('Tags').replace('>', ' ').replace('<', '')
-            redis.set(post['id'], tags)
-            post['body'] += ' ' + element.get('Title') + ' ' + tags
-        # If it is an answer, append parent tags
+            index = element.get('Id')
+            tags += element.get('Tags').replace('>', ' ').replace('<', '')
+            redis.set(index, tags)
         else:
             parent = element.get('ParentId')
             tags = redis.get(parent)
-            if type(tags) == str:
-                post['body'] += ' ' + tags
-        # Score filtering
+            if type(tags) != str:
+                tags = ''
+        # Filter score
         score = int(element.get('Score'))
-        if score >= 0:
-            extracted_count += 1
+        if score > 0:
+            # Get title
+            title = ' '
+            if post_type == 1:
+                title += element.get('Title')
+            # Get other information
+            post = {
+                'date': element.get('CreationDate')[:10],
+                'author': element.get('OwnerUserId'),
+                'body': element.get('Body') + title + tags
+            }
             # Write in CSV
             with open(path+results_file, 'a', errors='surrogatepass') as f:
                 writer = DictWriter(f, fieldnames=columns) 
                 writer.writerow(post)
+            extracted_count += 1
     # Clear memory
     element.clear()
     for ancestor in element.xpath('ancestor-or-self::*'):
