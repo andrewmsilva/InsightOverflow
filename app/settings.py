c9ffc25e80413441ae7e7e2e76904b60b09e208e
Andrew M. Silva <andrewsaxx@gmail.com>
2020-06-24 10:32:05 -0300

    Creating settings

---
+++ app/settings.py
@@ -0,0 +1,11 @@
+# Folders
+data_folder = '../data/'
+results_folder = '../results/'
+
+# Files
+posts_xml = 'posts.xml'
+posts_csv = 'posts>=0.csv'
+clean_posts_csv = 'clean-' + posts_csv
+enriched_post_csv = 'enriched-'  + posts_csv
+
+posts_header = ['date', 'author', 'body']
