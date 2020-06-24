7b58e9462b34d404b6bdee4932bbf33f883e99e4
Andrew M. Silva <andrewsaxx@gmail.com>
2020-06-24 11:03:36 -0300

    Fixing some mistakes

--- app/cleaning.py
+++ app/cleaning.py
@@ -28,13 +28,13 @@ def getPOS(token):
     return tag_dict.get(tag, wordnet.NOUN)
 
 # Create CSV
-with open(data_path+clean_posts_csv, 'w', errors='surrogatepass') as result_file:
+with open(data_folder+clean_posts_csv, 'w', errors='surrogatepass') as result_file:
     writer = DictWriter(result_file, fieldnames=posts_header) 
     writer.writeheader()
 
 # Clean posts
 word_net = WordNetLemmatizer()
-with open(data_path+posts_csv, "r") as csv_file:
+with open(data_folder+posts_csv, "r") as csv_file:
     for post in DictReader(csv_file):
         # Remove HTML tags
         soup = BeautifulSoup(post['body'], 'lxml')
@@ -45,7 +45,7 @@ with open(data_path+posts_csv, "r") as csv_file:
         post['body'] = [ word_net.lemmatize(token, pos=getPOS(token)) for token in simple_preprocess(post['body'], deacc=True) if token not in STOPWORDS ]
         post['body'] = ' '.join(post['body'])
         # Write in CSV
-        with open(data_path+clean_posts_csv, 'a', errors='surrogatepass') as result_file:
+        with open(data_folder+clean_posts_csv, 'a', errors='surrogatepass') as result_file:
             writer = DictWriter(result_file, fieldnames=posts_header) 
             writer.writerow(post)
 
