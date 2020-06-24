0c18b3b1dc62d17158fd232199f4fa07239c7fd0
Andrew M. Silva <andrewsaxx@gmail.com>
2020-06-24 15:57:49 -0300

    Fixing mistakes in data-enrichement

--- app/data-enrichment.py
+++ app/data-enrichment.py
@@ -1,6 +1,6 @@
 from settings import *
 
-from csv import DictReader
+from csv import DictWriter
 
 from gensim.models import Phrases
 from gensim.models.phrases import Phraser
@@ -12,7 +12,7 @@ start_time = time()
 # Train Phrases model
 bigram_model = Phrases(read_posts(clean_posts_csv, attribute='body', split=True), min_count=1)
 
-# create CSV
+# Create CSV
 with open(data_folder+clean_posts_csv, 'w', errors='surrogatepass') as result_file:
     writer = DictWriter(result_file, fieldnames=posts_header) 
     writer.writeheader()
@@ -23,8 +23,6 @@ for post in read_posts(clean_posts_csv):
     post['body'] = post['body'].split()
     bigrams = [ bigram for bigram in bigram_model[post['body']] if '_' in bigram ]
     post['body'] += bigrams
-    # Add post to dictionary
-    dictionary.add_documents([post['body']])
     # Write in CSV
     post['body'] = ' '.join(post['body'])
     with open(data_folder+enriched_post_csv, 'a', errors='surrogatepass') as csv_file:
