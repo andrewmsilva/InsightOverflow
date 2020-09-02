978232bc57577110973477a27a18e16e2ffe9315
Andrew M. Silva <andrewsaxx@gmail.com>
2020-09-02 09:43:24 -0300

    refactor(enrichment): changing terminology

--- app/enrichment.py
+++ app/enrichment.py
@@ -9,8 +9,8 @@ from gensim.corpora import Dictionary
 start_time = start_process('Enrichment')
 
 # Train Phrases model
-posts_stream = read_posts(clean_posts_csv, attribute='content', split=True)
-bigram_model = Phrases(posts_stream, min_count=1)
+posts = read_posts(clean_posts_csv, 'content', True)
+bigram_model = Phrases(posts, min_count=1)
 
 # Create CSV
 with open(data_folder+enriched_posts_csv, 'w', errors='surrogatepass') as result_file:
@@ -18,8 +18,8 @@ with open(data_folder+enriched_posts_csv, 'w', errors='surrogatepass') as result
     writer.writeheader()
 
 # Make bi-grams
-posts_stream = read_posts(clean_posts_csv)
-for post in posts_stream:
+posts = read_posts(clean_posts_csv)
+for post in posts:
     # Concatenate bi-grams
     post['content'] = post['content'].split()
     bigrams = [ bigram for bigram in bigram_model[post['content']] if '_' in bigram ]
