9ead92ca67968d337ceb351238dcfeee46bc6aee
Andrew M. Silva <andrewsaxx@gmail.com>
2020-09-04 15:48:25 -0300

    feat(RunStep): improving step choice

--- src/RunStep.py
+++ src/RunStep.py
@@ -1,16 +1,21 @@
 from modules.Extraction import Extraction
+from modules.PreProcessing import PreProcessing
+
+steps = [Extraction(), PreProcessing()]
 
 while True:
-    print('\n1 - Extraction')
-    print('2 - Pre-processing')
-    print('3 - Topic modeling')
+    print('\n')
+    for i in range(len(steps)):
+        print(i+1, '-', steps[i].stepName)
     print('0 - Exit\n')
+
     step = input('Choose a step to run: ')
 
     if step == '0':
         break
-    elif step == '1':
-        step = Extraction()
-        step.execute()
     else:
+        for i in range(len(steps)):
+            if str(i+1) == step:
+                steps[i].execute()
+                continue
         print('\nStep not found')
