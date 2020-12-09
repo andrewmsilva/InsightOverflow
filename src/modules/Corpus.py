5822b46802234ac1e1a5448e291669399bd733e6
Andrew M. Silva <andrewsaxx@gmail.com>
2020-12-09 09:11:51 -0300

    feat(Corpus): making dictionary and tf-idf fixed

--- src/modules/Corpus.py
+++ src/modules/Corpus.py
@@ -6,59 +6,40 @@ from gensim.models import TfidfModel
 class Corpus(object):
 
     def __init__(self):
+        # Load collection
         self.__contents = PreProcessedContents(splitted=True)
-
-        self.__dictionary = None
+        # Build dictionary
         self.__dictionaryFile = 'results/dictionary.bin'
-
-        self.__tfidf = None
+        try:
+            self.__dictionary = Dictionary.load(self.__dictionaryFile)
+        except:
+            self.__dictionary = Dictionary(self.__contents)
+            self.__dictionary.filter_extremes(no_below=int(0.05*len(self.__contents)), no_above=0.8, keep_n=100000)
+            self.__dictionary.save(self.__dictionaryFile)
+        # Build IF-IDF
         self.__tfidfFile = 'results/tfidf.bin'
+        try:
+            self.__tfidf = TfidfModel.load(self.__tfidfFile)
+        except:
+            self.__tfidf = TfidfModel(
+                self.__bow(),
+                id2word=self.__dictionary
+            )
+            self.__tfidf.save(self.__tfidfFile)
 
-        self.__len = None
-    
     def getDictionary(self):
         return self.__dictionary
     
     def getContents(self):
         return self.__contents
     
-    def build(self, no_below=None, no_above=None, keep_n=None):
-        self.__dictionary = Dictionary(self.__contents)
-
-        if no_below != None and no_above != None and keep_n != None:
-            self.__dictionary.filter_extremes(no_below=no_below, no_above=no_above, keep_n=keep_n)
-        
-        self.__tfidf = TfidfModel(
-            self.__bow(),
-            id2word=self.__dictionary
-        )
-    
     def __bow(self):
-        count = None
-        if not self.__len:
-            count = 0
-        
         for document in self.__contents:
-            if isinstance(count, int):
-                count += 1
             yield self.__dictionary.doc2bow(document)
-        
-        if isinstance(count, int):
-            self.__len = count
         
     def __iter__(self):
         for document in self.__bow():
             yield self.__tfidf[document]
 
     def __len__(self):
-        if not self.__len:
-            self.__len = len(self.__contents)
-        return self.__len
-    
-    def save(self):
-        self.__dictionary.save(self.__dictionaryFile)
-        self.__tfidf.save(self.__tfidfFile)
-    
-    def load(self):
-        self.__dictionary = Dictionary.load(self.__dictionaryFile)
-        self.__tfidf = TfidfModel.load(self.__tfidfFile)
+        return len(self.__contents)
