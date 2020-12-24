093f9f8c7c6783dcd0a58a41fbab1d1a5c886bb7
Andrew M. Silva <andrewsaxx@gmail.com>
2020-12-24 01:13:58 -0300

    bug(Step): adjusting execution time property

--- src/modules/Step.py
+++ src/modules/Step.py
@@ -23,12 +23,12 @@ class Step(object):
         startTime = time()
         self._process()
         endTime = time()
-        self.executionTime = endTime - startTime
+        self.__executionTime = endTime - startTime
         print('Execution time:', self.getFormatedExecutionTime())
     
     def getFormatedExecutionTime(self):
-        if self.executionTime:
-            hours, rem = divmod(self.executionTime, 3600)
+        if self.__executionTime:
+            hours, rem = divmod(self.__executionTime, 3600)
             minutes, seconds = divmod(rem, 60)
             return '{:0>2}:{:0>2}:{:05.2f}'.format(int(hours), int(minutes), seconds)
         else:
