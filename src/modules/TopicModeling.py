8ec10944148ae09d6d31b11ef358aef16b4fb942
Andrew M. Silva <andrewsaxx@gmail.com>
2020-12-26 16:05:12 -0300

    feat(TopicModeling): updating experiments

--- src/modules/TopicModeling.py
+++ src/modules/TopicModeling.py
@@ -1,4 +1,4 @@
-from modules.Step import Step
+from modules.Step import Step, time
 from modules.Corpus import Corpus
 from modules.TopicModel import TopicModel
 
@@ -9,52 +9,64 @@ class TopicModeling(Step):
     def __init__(self):
         super().__init__('Topic modeling')
 
-        self.__corpus = Corpus()
+        self.__corpus = None
         self.__model = TopicModel()
 
-        self.__experiments = pd.DataFrame(columns=['no_below', 'no_above', 'keep_n', 'num_topics', 'model_name', 'coherence'])
+        self.__experiments = pd.DataFrame(columns=['model_name', 'num_topics', 'chunksize', 'passes', 'iterations', 'execution_time', 'coherence'])
         self.__experimentsFile = 'results/experiments.csv'
 
     # Experiment methods
 
-    def __saveExperiment(self, no_below, no_above, keep_n, num_topics, model_name, coherence):
+    def __formatExecutionTime(self, execution_time):
+        step = Step()
+        step.setExcecutionTime(execution_time)
+        return step.getFormatedExecutionTime()
+    
+    def __buildCorpus(self):
+        start_time = time()
+        self.__corpus = Corpus()
+        execution_time = self.__formatExecutionTime(time()-start_time)
+        print('  Corpus built: {}'.format(execution_time))
+
+    def __saveExperiment(self, model_name, num_topics, chunksize, passes, iterations, execution_time, coherence):
         # Save model with greatest coherence
         if self.__experiments.empty or self.__experiments.iloc[self.__experiments['coherence'].idxmax()]['coherence'] < coherence:
-            self.__corpus.save()
-            self.__model.save()
+            self.__model.save()        
         # Save experiment to CSV
+        execution_time = self.__formatExecutionTime(execution_time)
         row = {
-            'no_below': no_below,
-            'no_above': no_above,
-            'keep_n': keep_n,
-            'num_topics': num_topics,
             'model_name': model_name,
+            'num_topics': num_topics,
+            'chunksize': chunksize,
+            'passes': passes,
+            'iterations': iterations,
+            'execution_time': execution_time,
             'coherence': coherence
         }
         self.__experiments = self.__experiments.append(row, ignore_index=True)
         self.__experiments.to_csv(self.__experimentsFile)
     
-    def _process(self):
-        # Dictionary parameters
-        no_below_list = [200]
-        no_above_list = [0.2, 0.4]
-        keep_n_list = [1000, 2000, 4000]
-        # Topic model parameters
-        num_topics_list = [10, 20, 40, 60, 80]
+        print('  Experiment done: {}, {}, {}, {}, {} | {}, {:.4f}'.format(model_name, num_topics, chunksize, passes, iterations, execution_time, coherence))
+    
+    def __runExperiments(self):
+        chunksize_list = [5000, 50000, 500000]
+        passes_list = [10, 100]
+        iterations_list = [100, 200]
+        num_topics_list = [20, 40, 60, 80]
         model_name_list = ['lda', 'nmf']
         # Starting experiments
-        count = 0
-        for no_below in no_below_list:
-            for no_above in no_above_list:
-                for keep_n in keep_n_list:
-                    self.__corpus.build(no_below, no_above, keep_n)
+        for chunksize in chunksize_list:
+            for passes in passes_list:
+                for iterations in iterations_list:
                     for num_topics in num_topics_list:
                         for model_name in model_name_list:
+                            start_time = time()
                             # Build topic model and compute coherence
-                            self.__model.build(model_name, num_topics, self.__corpus)
+                            self.__model.build(model_name, num_topics, chunksize, passes, iterations, self.__corpus)
                             coherence = self.__model.getCoherence()
                             # Save experiment to file
-                            self.__saveExperiment(no_below, no_above, keep_n, num_topics, model_name, coherence)
-                            # Print some results
-                            print('Experiment', count, 'presented coherence', coherence)
-                            count += 1
+                            self.__saveExperiment(model_name, num_topics, chunksize, passes, iterations, start_time+time(), coherence)
+
+    def _process(self):
+        self.__buildCorpus()
+        self.__runExperiments()
