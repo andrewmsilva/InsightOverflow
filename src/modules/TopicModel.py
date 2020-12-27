3a0c448b75e2d01ab8fdc16f32b29227a5ea2fcb
Andrew M. Silva <andrewsaxx@gmail.com>
2020-12-27 16:02:25 -0300

    feat(TopicModel): removing iterations from experiments

--- src/modules/TopicModel.py
+++ src/modules/TopicModel.py
@@ -22,40 +22,38 @@ class TopicModel(object):
     def getDocumentTopics(self, document, threshold=None):
         return self.__model.get_document_topics(document, threshold)
     
-    def build(self, model_name, num_topics, chunksize, passes, iterations, corpus=None):
+    def build(self, model_name, num_topics, chunksize, passes, corpus=None):
         self.__modelName = model_name
         # Update corpus if necessary
         if corpus:
             self.__corpus = corpus
         # Build topic model
         if model_name == 'lda':
-            self.__buildLDA(num_topics, chunksize, passes, iterations)
+            self.__buildLDA(num_topics, chunksize, passes)
         elif model_name == 'nmf':
-            self.__buildNMF(num_topics, chunksize, passes, iterations)
+            self.__buildNMF(num_topics, chunksize, passes)
         # Build coherence model
         self.__buildCoherenceModel()
     
-    def __buildLDA(self, num_topics, chunksize, passes, iterations):                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+    def __buildLDA(self, num_topics, chunksize, passes):                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
         self.__model = LdaMulticore(
             self.__corpus,
             id2word=self.__corpus.getDictionary(),
             num_topics=num_topics,
             chunksize=chunksize,
             passes=passes,
-            iterations=iterations,
             eval_every=None,
             workers=7,
             random_state=10
         )
     
-    def __buildNMF(self, num_topics, chunksize, passes, iterations):
+    def __buildNMF(self, num_topics, chunksize, passes):
         self.__model = Nmf(
             self.__corpus,
             id2word=self.__corpus.getDictionary(),
             num_topics=num_topics,
             chunksize=chunksize,
             passes=passes,
-            iterations=iterations,
             eval_every=None,
             random_state=10
         )
