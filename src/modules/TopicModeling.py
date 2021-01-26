30ee6947f63ea7463086c41ff7fcd4d73c90ce98
Andrew M. Silva <andrewsaxx@gmail.com>
2021-01-25 22:55:21 -0300

    refactor(ToicModeling): removing corpus saving

--- src/modules/TopicModeling.py
+++ src/modules/TopicModeling.py
@@ -12,10 +12,10 @@ class TopicModeling(Step):
     def __init__(self):
         super().__init__('Topic modeling')
 
-        self.__corpusFile = 'results/corpus.bin'
         self.__corpus = None
 
         self.__modelFile = 'results/model.bin'
+        self.__model = None
 
         self.__experimentsFile = 'results/experiments.csv'
         self.__experiments = None
@@ -29,13 +29,9 @@ class TopicModeling(Step):
     
     def __buildCorpus(self):
         start_time = time()
-        try:
-            self.__corpus = tp.utils.Corpus.load(self.__corpusFile)
-        except:
-            self.__corpus = tp.utils.Corpus()
-            for post in PreProcessedContents(splitted=True):
-                self.__corpus.add_doc(post)
-            self.__corpus.save(self.__corpusFile)
+        self.__corpus = tp.utils.Corpus()
+        for post in PreProcessedContents(splitted=True):
+            self.__corpus.add_doc(post)
         execution_time = self.__formatExecutionTime(time()-start_time)
         print('  Corpus built: {}'.format(execution_time))
     
@@ -47,23 +43,23 @@ class TopicModeling(Step):
         for iterations in range(10, max_iterations+1, 10):
             for num_topics in range(10, max_topics+1, 10):
                 # Train and load model
-                model = tp.LDAModel(corpus=self.__corpus, k=num_topics, min_df=200, rm_top=20, seed=10)
-                model.train(iter=iterations, workers=50)
+                self.__model = tp.LDAModel(corpus=self.__corpus, k=num_topics, min_df=200, rm_top=20, seed=10)
+                self.__model.train(iter=iterations, workers=50)
                 # Compute c_v coherence
-                cv = tp.coherence.Coherence(model, coherence='c_v')
+                cv = tp.coherence.Coherence(self.__model, coherence='c_v')
                 # Save experiment
-                self.__saveExperiment(model, model.global_step, model.k, model.perplexity, cv.get_score())
+                self.__saveExperiment(cv.get_score())
         
-    def __saveExperiment(self, model, iterations, num_topics, perplexity, coherence):
+    def __saveExperiment(self, coherence):
         # Save model with greatest coherence
         if self.__experiments.empty or self.__experiments.iloc[self.__experiments['coherence'].idxmax()]['coherence'] < coherence:
-            model.save(self.__modelFile, full=False)
+            self.__model.save(self.__modelFile, full=False)
         
         # Save experiment to CSV
         row = {
-            'iterations': iterations,
-            'num_topics': num_topics,
-            'perplexity': perplexity,
+            'iterations': self.__model.global_step,
+            'num_topics': self.__model.k,
+            'perplexity': self.__model.perplexity,
             'coherence': coherence
         }
 
