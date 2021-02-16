ec67c8154d748a80ab07af7e5240d1e92ba0b6ae
Andrew M. Silva <andrewsaxx@gmail.com>
2021-02-16 11:13:01 -0300

    feat(TopicModeling): trying continuous iterations

--- src/modules/TopicModeling.py
+++ src/modules/TopicModeling.py
@@ -26,30 +26,30 @@ class TopicModeling(Step):
             if len(post) > 0:
                 model.add_doc(post)
     
-    def __trainModel(self, iterations, num_topics):
+    def __trainModel(self, num_topics):
         # Load experiments
         experiments = pd.read_csv(self.__experimentsFile, index_col=0, header=0)
         # Running if this is a new experiment
-        if not ((experiments['iterations'] == iterations) & (experiments['num_topics'] == num_topics)).any():
-            start_time = time()
+        if not (experiments['num_topics'] == num_topics).any():
             # Create model and add corpus
             model = tp.LDAModel(k=num_topics, min_df=200, rm_top=20, seed=10)
             self.__addCorpus(model)
-            # Train model
-            model.train(iter=iterations, workers=40)
-            # Compute c_v coherence
-            cv = tp.coherence.Coherence(model, coherence='c_v')
-            coherence = cv.get_score()       
-            # Save model if it has the greatest coherence
-            if experiments.empty or experiments.iloc[experiments['coherence'].idxmax()]['coherence'] < coherence:
-                model.save(self.__modelFile)
-            # Save experiment
-            execution_time = self.__formatExecutionTime(time()-start_time)
-            row = [model.global_step, model.k, execution_time, model.perplexity, coherence]
-            experiments = experiments.append(dict(zip(experiments.columns, row)), ignore_index=True)
-            experiments.to_csv(self.__experimentsFile)
-            # Print result
-            print('  Experiment done: i={} k={} t={} p={:.2f} cv={:.2f}'.format(row[0], row[1],row[2], row[3], row[4]))  
+            # Iterate
+            for iterations in range(10, 101, 10):
+                # Train model
+                model.train(iter=10, workers=40)
+                # Compute c_v coherence
+                cv = tp.coherence.Coherence(model, coherence='c_v')
+                coherence = cv.get_score()       
+                # Save model if it has the greatest coherence
+                if experiments.empty or experiments.iloc[experiments['coherence'].idxmax()]['coherence'] < coherence:
+                    model.save(self.__modelFile)
+                # Save experiment
+                row = [model.k, model.global_step, model.perplexity, coherence]
+                experiments = experiments.append(dict(zip(experiments.columns, row)), ignore_index=True)
+                experiments.to_csv(self.__experimentsFile)
+                # Print result
+                print('  Experiment done: k={} i={} p={:.2f} cv={:.2f}'.format(row[0], row[1], row[2], row[3]))  
             # Clear memory
             del model, cv, experiments
             gc.collect()
@@ -60,21 +60,19 @@ class TopicModeling(Step):
         try:
             experiments = pd.read_csv(self.__experimentsFile, index_col=0, header=0)
         except:
-            experiments = pd.DataFrame(columns=['iterations', 'num_topics', 'execution_time', 'perplexity', 'coherence'])
+            experiments = pd.DataFrame(columns=['num_topics', 'iterations', 'perplexity', 'coherence'])
             experiments.to_csv(self.__experimentsFile)
         # Run experiments
-        max_iterations = 1000
         max_topics = 100
-        for iterations in range(10, max_iterations+1, 10):
-            for num_topics in range(10, max_topics+1, 10):
-                p = Process(target=self.__trainModel, args=(iterations, num_topics))
-                p.start()
-                p.join()
-                p.terminate()
-                # Clear memory
-                del p
-                gc.collect()
+        for num_topics in range(10, max_topics+1, 10):
+            p = Process(target=self.__trainModel, args=(num_topics))
+            p.start()
+            p.join()
+            p.terminate()
+            # Clear memory
+            del p
+            gc.collect()
         # Print best experiment
         experiments = pd.read_csv(self.__experimentsFile, index_col=0, header=0)
         best = experiments.iloc[experiments['coherence'].idxmax()]
-        print('  Best experiment: i={} k={} t={} p={:.2f} cv={:.2f}'.format(best['iterations'], best['num_topics'], best['execution_time'], best['perplexity'], best['coherence']))      
+        print('  Best experiment: i={} k={} p={:.2f} cv={:.2f}'.format(best['num_topics'], best['iterations'], best['perplexity'], best['coherence']))      
