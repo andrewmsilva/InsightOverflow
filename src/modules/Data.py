b1b54292fe6e36a48b8a13d60cc083dbcf73e943
Andrew M. Silva <andrewsaxx@gmail.com>
2021-02-27 23:31:29 -0300

    refactor: changing data stream strategy

--- src/modules/Data.py
+++ src/modules/Data.py
@@ -1,62 +1,62 @@
-from smart_open import open
 
 class Stream(object):
-
-    def __init__(self, output_file, overwrite=False, splitted=False):
-        self.__length = None
-        self.__outputFile = output_file
-        self.__overwrite = overwrite
-        self.__splitted = splitted
+    def __init__(self, textFile, maxLen=None):
+        self.__textFile = textFile
+        self.__maxLen = maxLen
+        self.__len = None
+        self.__data = None
+    
+    def __loadData(self):
+        self.__data = []
+        with open(self.__textFile, "r") as f:
+            self.__len = 0
+            for row in f:
+                if not self.__maxLen or self.__maxLen < self.__len:
+                    self.__data.append(row)
+                    self.__len += 1
+    
+    def append(self, row):
+        mode = "a"
+        if not self.__data:
+            mode = "w"
+            self.__data = []
+            self.__len = 0
+        
+        with open(self.__textFile, mode, errors="surrogatepass") as f:
+            f.write(str(row)+'\n')
+            self.__data.append(row)
+            self.__len += 1
 
     def __iter__(self):
-        with open(self.__outputFile, "r") as txt_file:
-            count = None
-            if not self.__length:
-                count = 0
-
-            for data in txt_file:
-                if count != None:
-                    count += 1
-                try:
-                    yield int(data)
-                except:
-                    if self.__splitted:
-                        yield data.split()
-                    else:
-                        yield data
-            if count != None:
-                self.__length = count
+        if not self.__data:
+            self.__loadData()
+        
+        for item in data:
+            yield data
+    
+    def __getitem__(self, key):
+        return self.__data[key]
     
     def __len__(self):
-        if not self.__length:
-            self.__length = 0  
-            for data in self:
-                self.__length += 1
-        return self.__length
+        if not self.__len:
+            self.__loadData()
+        return self.__len
+
+class Posts(object):
+
+    def __init__(self, preProcessed=False, maxLen=None):
+        self.users = Stream("data/users.txt", maxLen)
+        self.dates = Stream("data/dates.txt", maxLen)
+
+        if preProcessed:
+            self.contents = Stream("data/data/pre-processed-contents.txt", maxLen)
+        else:
+            self.contents = Stream("data/contents.txt", maxLen)
     
-    def append(self, data):
-        mode = 'a'
-        if self.__overwrite:
-            self.__overwrite = False
-            self.__length = 0
-            mode = 'w'
-
-        with open(self.__outputFile, mode, errors='surrogatepass') as txt_file:
-            txt_file.write(str(data)+'\n')
-            self.__length += 1
-
-class Users(Stream):
-    def __init__(self, overwrite=False, splitted=False):
-        super().__init__('data/users.txt', overwrite, splitted)
-
-class Dates(Stream):
-    def __init__(self, overwrite=False, splitted=False):
-        super().__init__('data/dates.txt', overwrite, splitted)
-
-class Contents(Stream):
-    def __init__(self, overwrite=False, splitted=False):
-        super().__init__('data/contents.txt', overwrite, splitted)
-
-class PreProcessedContents(Stream):
-    def __init__(self, overwrite=False, splitted=False):
-        super().__init__('data/pre-processed-contents.txt', overwrite, splitted)
+    def __iter__(self):
+        for (content, user, date) in zip(self.__contents, self.__users, self.__dates):
+            yield {
+                'content': content,
+                'user': user,
+                'date': date,
+            }
