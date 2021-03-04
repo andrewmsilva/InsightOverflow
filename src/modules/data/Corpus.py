1104dc30705a36bbb1faf90f62ee8d3c6574a022
Andrew M. Silva <andrewsaxx@gmail.com>
2021-03-04 18:40:46 -0300

    feat: building and iterating corpus

--- src/modules/data/Corpus.py
+++ src/modules/data/Corpus.py
@@ -1,4 +1,5 @@
-from modules.Data import PreProcessedContents
+from .BaseStream import BaseStream
+from .Posts import Posts
 
 from gensim.corpora import Dictionary
 from gensim.models import TfidfModel
@@ -6,18 +7,21 @@ from gensim.models import TfidfModel
 class Corpus(object):
 
     def __init__(self):
-        # Load collection
-        self.__contents = PreProcessedContents(splitted=True)
-        # Build dictionary
+        self.__posts = Posts(preProcessed=True, maxLen=10000)
+        self.__corpus = None
+        
         self.__dictionaryFile = 'results/dictionary.bin'
+        self.__tfidfFile = 'results/tfidf.bin'
+    
+    def __buildDictionary(self):
         try:
             self.__dictionary = Dictionary.load(self.__dictionaryFile)
         except:
-            self.__dictionary = Dictionary(self.__contents)
-            self.__dictionary.filter_extremes(no_below=int(0.05*len(self.__contents)), no_above=0.8, keep_n=100000)
+            self.__dictionary = Dictionary(self.__posts.contents)
+            self.__dictionary.filter_extremes(no_below=int(0.05*len(self.__posts.contents)), no_above=0.8, keep_n=10000)
             self.__dictionary.save(self.__dictionaryFile)
-        # Build IF-IDF
-        self.__tfidfFile = 'results/tfidf.bin'
+    
+    def __buildTFIDF(self):
         try:
             self.__tfidf = TfidfModel.load(self.__tfidfFile)
         except:
@@ -26,20 +30,33 @@ class Corpus(object):
                 id2word=self.__dictionary
             )
             self.__tfidf.save(self.__tfidfFile)
+    
+    def __bow(self):
+        for doc in self.__posts.contents:
+            yield self.__dictionary.doc2bow(doc)
+    
+    def build(self):
+        self.__buildDictionary()
+        self.__buildTFIDF()
+
+        self.__corpus = []
+        for doc in self.__bow():
+            self.__corpus.append(self.__tfidf[doc])
 
     def getDictionary(self):
         return self.__dictionary
     
-    def getContents(self):
-        return self.__contents
+    def getTexts(self):
+        return self.__posts.contents
     
-    def __bow(self):
-        for document in self.__contents:
-            yield self.__dictionary.doc2bow(document)
-        
     def __iter__(self):
-        for document in self.__bow():
-            yield self.__tfidf[document]
+        if not self.__corpus:
+            self.build()
+        for doc in self.__bow():
+            yield self.__tfidf[doc]
+
+    def __getitem__(self, key):
+        return self.__corpus[key]
 
     def __len__(self):
-        return len(self.__contents)
+        return len(self.__posts)
